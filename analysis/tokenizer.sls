ufo5260987423/scheme-langserver

commit 6e7d309543387971e9bef6e08f62a1aa32df1d80

feat(tokenizer): per-opener diagnoses for unclosed brackets
- Add private:find-unclosed-parens to scan source and find all unclosed (/
  [- Add private:append-unclosed-paren-diagnoses to generate separate
    diagnoses for each unclosed opener
- For EOF errors (unexpected end-of-file reading ~a), suppress the generic
  EOF diagnose and emit per-opener diagnoses instead
- For cross-bracket mismatch, keep original diagnose + append unclosed ones
- Change recursive tolerant call from #f to #t so multiple independent
  errors in a file all get diagnosed
- Add max-depth guard (default 10) to prevent infinite tolerant loops
- Move string-find-delimiter, consume-sps-auxiliary and
  consume-block-comment before their use sites to fix forward refs
- Fix test-tokenizer.sps: run.ss has 17 top-level expressions, not 16
- Update test-tokenizer-diagnose.sps expectations to match new per-opener
  diagnose counts
- Add test-tokenizer-diagnoses.sps with 11 new assertions

analysis/tokenizer.sls
@@ -46,6 +46,26 @@
       [rest (string-take-right source (max 0 (- (string-length source) position length)))])
     (string-append head (make-string (max 0 length) #\space) rest)))
 
+; Helper: find next delimiter position in string
+(define (string-find-delimiter s position)
+  (cond
+    [(>= position (string-length s)) (string-length s)]
+    [else
+      (case (string-ref s position)
+        [#\( position]
+        [#\) position]
+        [#\[ position]
+        [#\] position]
+        [#\" position];"
+        [#\; position]
+        [#\# position]
+        [#\space position]
+        [#\newline position]
+        [#\linefeed position]
+        [#\tab position]
+        [#\return position]
+        [else (string-find-delimiter s (+ 1 position))])]))
+
 ; Helper: replace token starting at position up to (but not including) the next delimiter
 (define (private:replace-token source position)
   (let ([end (string-find-delimiter source (+ 1 position))])
@@ -344,56 +364,50 @@
              [else (+ start 1)])])
     `(,start ,(min end (string-length source)) 1 ,(string-append "Syntax error: " msg))))
 
-(define source-file->annotations
-  (case-lambda
-    ([path] (source-file->annotations (read-string path) path))
-    ([source path] (source-file->annotations source path (consume-sps-auxiliary source) #t))
-    ([source path start-position] (source-file->annotations source path start-position #t))
-    ([source path start-position tolerant?]
-      (source-file->annotations source path start-position tolerant? #f))
-    ([source path start-position tolerant? maybe-document]
-      (if (file-exists? path)
-        (let ([port (open-string-input-port source)]
-            [source-file-descriptor (make-source-file-descriptor path (open-file-input-port path))])
-          (set-port-position! port start-position)
-          (filter annotation? 
-            (let loop ([position start-position])
-              (try
-                (let-values ([(ann end-pos) (get-datum/annotations port source-file-descriptor position)]) 
-                  (if (= position (port-position port))
-                    '()
-                    `(,ann . ,(loop (port-position port)))))
-                (except e
-                  [(and tolerant? (condition? e))
-                    (let ([error-position (private:compute-error-position e port)])
-                      (when maybe-document
-                        (append-new-diagnoses maybe-document (append (private:condition->diagnose e source error-position) '("syntax" "syntax-error"))))
-                      (let ([after (private:tolerant-parse->patch source error-position)])
-                        (if (= (string-length after) (string-length source))
-                          (source-file->annotations after path start-position #f maybe-document)
-                          (error 'tokenizer-error (condition-message e) (condition-irritants e)))))]
-                  [(condition? e)
-                    (let ([error-position (private:compute-error-position e port)])
-                      (when maybe-document
-                        (append-new-diagnoses maybe-document (append (private:condition->diagnose e source error-position) '("syntax" "syntax-error"))))
-                      (error 'tokenizer-error0 path `(,source ,path ,error-position ,tolerant? ,(condition-who e) ,(condition-message e) ,(condition-irritants e))))]
-                  [else 
-                    (let ([error-position (max 0 (- (port-position port) 1))])
-                      (when maybe-document
-                        (append-new-diagnoses maybe-document `(,error-position ,(+ error-position 1) 1 "Syntax error: unknown parse error" "syntax" "syntax-error")))
-                      (warning 'tokenizer-error0 path `(,source ,path ,error-position ,tolerant?))
-                      '())])))))
-          (begin
-            (when maybe-document
-              (append-new-diagnoses maybe-document `(0 0 1 ,(string-append "File not found: " path) "syntax" "file-not-found")))
-            (warning 'no-such-file-warning path '())
-            '())))))
+(define (private:find-unclosed-parens source)
+  (let loop ([i 0] [stack '()])
+    (cond
+      [(>= i (string-length source)) (reverse stack)]
+      [(char=? #\( (string-ref source i))
+       (loop (+ i 1) (cons `(,i #\() stack))]
+      [(char=? #\[ (string-ref source i))
+       (loop (+ i 1) (cons `(,i #\[) stack))]
+      [(char=? #\) (string-ref source i))
+       (if (and (pair? stack) (char=? #\( (cadar stack)))
+         (loop (+ i 1) (cdr stack))
+         (loop (+ i 1) stack))]
+      [(char=? #\] (string-ref source i))
+       (if (and (pair? stack) (char=? #\[ (cadar stack)))
+         (loop (+ i 1) (cdr stack))
+         (loop (+ i 1) stack))]
+      [else (loop (+ i 1) stack)])))
 
-;https://github.com/cisco/ChezScheme/blob/e63e5af1a5d6805c96fa8977e7bd54b3b516cff6/s/7.ss#L268-L280
-; consume
-; #!/usr/bin/env scheme-script
-; #!r6rs
-; #!...
+(define (private:append-unclosed-paren-diagnoses document source msg)
+  (let ([unclosed (private:find-unclosed-parens source)])
+    (for-each
+      (lambda (item)
+        (let ([pos (car item)])
+          (append-new-diagnoses document
+            `(,pos ,(+ pos 1) 1
+              ,(string-append "Syntax error: unclosed "
+                (if (char=? #\( (cadr item)) "parenthesis" "bracket"))
+              "syntax" "syntax-error"))))
+      unclosed)))
+
+; block comment: #| ... |#
+; may be nested
+(define (consume-block-comment char-input-port)
+  (let loop ([c (get-char char-input-port)])
+    (cond
+      [(and (eqv? c #\|) (eqv? (lookahead-char char-input-port) #\#))
+        (get-char char-input-port) 
+        (port-position char-input-port)]
+      [(and (eqv? c #\#) (eqv? (lookahead-char char-input-port) #\|))
+        (get-char char-input-port) 
+        (consume-block-comment char-input-port)]
+      [(eof-object? c) (port-position char-input-port)]
+      [else (loop (get-char char-input-port))])))
+
 ; line comment: ; ... 
 ; don't need consume datum comment  
 (define (consume-sps-auxiliary source)
@@ -416,36 +430,69 @@
         [(and (not inline-comment?) (eqv? c #\( )) (- (port-position ip) 1)]
         [else (loop (get-char ip) inline-comment?)]))))
 
-; block comment: #| ... |#
-; may be nested
-(define (consume-block-comment char-input-port)
-  (let loop ([c (get-char char-input-port)])
-    (cond
-      [(and (eqv? c #\|) (eqv? (lookahead-char char-input-port) #\#))
-        (get-char char-input-port) 
-        (port-position char-input-port)]
-      [(and (eqv? c #\#) (eqv? (lookahead-char char-input-port) #\|))
-        (get-char char-input-port) 
-        (consume-block-comment char-input-port)]
-      [(eof-object? c) (port-position char-input-port)]
-      [else (loop (get-char char-input-port))])))
+(define source-file->annotations
+  (case-lambda
+    ([path] (source-file->annotations (read-string path) path))
+    ([source path] (source-file->annotations source path (consume-sps-auxiliary source) #t))
+    ([source path start-position] (source-file->annotations source path start-position #t))
+    ([source path start-position tolerant?]
+      (source-file->annotations source path start-position tolerant? #f))
+    ([source path start-position tolerant? maybe-document]
+      (source-file->annotations source path start-position tolerant? maybe-document 10))
+    ([source path start-position tolerant? maybe-document max-depth]
+      (if (file-exists? path)
+        (let ([port (open-string-input-port source)]
+            [source-file-descriptor (make-source-file-descriptor path (open-file-input-port path))])
+          (set-port-position! port start-position)
+          (filter annotation? 
+            (let loop ([position start-position])
+              (try
+                (let-values ([(ann end-pos) (get-datum/annotations port source-file-descriptor position)]) 
+                  (if (= position (port-position port))
+                    '()
+                    `(,ann . ,(loop (port-position port)))))
+                (except e
+                  [(and tolerant? (condition? e))
+                    (let ([error-position (private:compute-error-position e port)])
+                      (when maybe-document
+                        (let ([msg (condition-message e)])
+                          (if (private:message-matches? msg "unexpected end-of-file reading ~a")
+                            (private:append-unclosed-paren-diagnoses maybe-document source msg)
+                            (begin
+                              (append-new-diagnoses maybe-document (append (private:condition->diagnose e source error-position) '("syntax" "syntax-error")))
+                              (when (or (private:message-matches? msg "parenthesized list terminated by bracket")
+                                        (private:message-matches? msg "bracketed list terminated by parenthesis"))
+                                (private:append-unclosed-paren-diagnoses maybe-document source msg))))))
+                      (let ([after (private:tolerant-parse->patch source error-position)])
+                        (if (= (string-length after) (string-length source))
+                          (if (> max-depth 0)
+                            (source-file->annotations after path start-position #t maybe-document (- max-depth 1))
+                            (begin
+                              (when maybe-document
+                                (append-new-diagnoses maybe-document
+                                  `(,error-position ,(+ error-position 1) 1 "Syntax error: max tolerant depth exceeded" "syntax" "syntax-error")))
+                              '()))
+                          (error 'tokenizer-error (condition-message e) (condition-irritants e)))))]
+                  [(condition? e)
+                    (let ([error-position (private:compute-error-position e port)])
+                      (when maybe-document
+                        (append-new-diagnoses maybe-document (append (private:condition->diagnose e source error-position) '("syntax" "syntax-error"))))
+                      (error 'tokenizer-error0 path `(,source ,path ,error-position ,tolerant? ,(condition-who e) ,(condition-message e) ,(condition-irritants e))))]
+                  [else 
+                    (let ([error-position (max 0 (- (port-position port) 1))])
+                      (when maybe-document
+                        (append-new-diagnoses maybe-document `(,error-position ,(+ error-position 1) 1 "Syntax error: unknown parse error" "syntax" "syntax-error")))
+                      (warning 'tokenizer-error0 path `(,source ,path ,error-position ,tolerant?))
+                      '())])))))
+          (begin
+            (when maybe-document
+              (append-new-diagnoses maybe-document `(0 0 1 ,(string-append "File not found: " path) "syntax" "file-not-found")))
+            (warning 'no-such-file-warning path '())
+            '()))))))
 
-(define (string-find-delimiter s position)
-  (cond
-    [(>= position (string-length s)) (string-length s)]
-    [else
-      (case (string-ref s position)
-        [#\( position]
-        [#\) position]
-        [#\[ position]
-        [#\] position]
-        [#\" position];"
-        [#\; position]
-        [#\# position]
-        [#\space position]
-        [#\newline position]
-        [#\linefeed position]
-        [#\tab position]
-        [#\return position]
-        [else (string-find-delimiter s (+ 1 position))])]))
-)
+;https://github.com/cisco/ChezScheme/blob/e63e5af1a5d6805c96fa8977e7bd54b3b516cff6/s/7.ss#L268-L280
+; consume
+; #!/usr/bin/env scheme-script
+; #!r6rs
+; #!...
+

tests/analysis/test-tokenizer-diagnose.sps
@@ -33,7 +33,7 @@
       '())))
 
 (test-equal "source-file->annotations with document adds diagnose for unmatched paren"
-  1
+  2
   (guard (e [else 
               (pretty-print `(EXCEPTION ,(condition-message e) ,(condition-irritants e)))
               -1])
@@ -59,7 +59,7 @@
         (string-prefix? "Syntax error:" msg)))))
 
 (test-equal "init-document adds diagnose for syntax error"
-  1
+  2
   (guard (e [else 
               (pretty-print `(EXCEPTION ,(condition-message e) ,(condition-irritants e)))
               -1])
@@ -70,7 +70,7 @@
         result))))
 
 (test-equal "update-file-node-with-tail clears old diagnose and adds new"
-  1
+  2
   (guard (e [else 
               (pretty-print `(EXCEPTION ,(condition-message e) ,(condition-irritants e)))
               -1])
@@ -98,7 +98,7 @@
       (let ([d (file-node-document target)])
         (refresh-workspace-for workspace target)
         (let ([diagnoses (document-diagnoses d)])
-          (test-equal "syntax diagnose preserved after refresh" 1 (length diagnoses))
+          (test-equal "syntax diagnose preserved after refresh" 2 (length diagnoses))
           (string-prefix? "Syntax error:" (cadddr (car diagnoses))))))))
 
 (test-end)

tests/analysis/test-tokenizer-diagnoses.sps
@@ -1,0 +1,78 @@
+#!/usr/bin/env scheme-script
+;; -*- mode: scheme; coding: utf-8 -*- !#
+;; Copyright (c) 2022 WANG Zheng
+;; SPDX-License-Identifier: MIT
+#!r6rs
+
+(import 
+    (chezscheme)
+    (srfi :64 testing)
+    (scheme-langserver analysis tokenizer)
+    (scheme-langserver virtual-file-system document))
+
+(define (string-contains? substr str)
+  (let ([sub-len (string-length substr)]
+        [str-len (string-length str)])
+    (let loop ([i 0])
+      (cond
+        [(> i (- str-len sub-len)) #f]
+        [(string=? substr (substring str i (+ i sub-len))) #t]
+        [else (loop (+ i 1))]))))
+
+(test-begin "tokenizer-diagnoses")
+
+(test-begin "nested-unclosed-parens")
+(let ([doc (make-document "file:///test.ss" "(a (b (c" '())])
+  (call-with-port (open-file-output-port "/tmp/test-tokenizer/test.ss" (file-options replace))
+    (lambda (p) (put-bytevector p (string->utf8 "(a (b (c"))))
+  (source-file->annotations "(a (b (c" "/tmp/test-tokenizer/test.ss" 0 #t doc)
+  (let ([diagnoses (document-diagnoses doc)])
+    (test-equal 3 (length diagnoses))
+    (test-equal 3
+      (length (filter (lambda (d) (string-contains? "unclosed parenthesis" (list-ref d 3))) diagnoses)))))
+(test-end)
+
+(test-begin "extra-close-paren")
+(let ([doc (make-document "file:///test.ss" "(foo (bar)))" '())])
+  (call-with-port (open-file-output-port "/tmp/test-tokenizer/test.ss" (file-options replace))
+    (lambda (p) (put-bytevector p (string->utf8 "(foo (bar)))"))))
+  (source-file->annotations "(foo (bar)))" "/tmp/test-tokenizer/test.ss" 0 #t doc)
+  (let ([diagnoses (document-diagnoses doc)])
+    (test-equal 1 (length diagnoses))
+    (test-assert
+      (find (lambda (d) (string-contains? "unexpected close parenthesis" (list-ref d 3))) diagnoses))))
+(test-end)
+
+(test-begin "multiple-independent-errors")
+(let ([doc (make-document "file:///test.ss" "(a b]\n(c d]" '())])
+  (call-with-port (open-file-output-port "/tmp/test-tokenizer/test.ss" (file-options replace))
+    (lambda (p) (put-bytevector p (string->utf8 "(a b]\n(c d]"))))
+  (source-file->annotations "(a b]\n(c d]" "/tmp/test-tokenizer/test.ss" 0 #t doc)
+  (let ([diagnoses (document-diagnoses doc)])
+    ; 1 original cross-bracket + 2 unclosed parens
+    (test-equal 3 (length diagnoses))
+    (test-equal 1
+      (length (filter (lambda (d) (string-contains? "parenthesized list terminated by bracket" (list-ref d 3))) diagnoses)))
+    (test-equal 2
+      (length (filter (lambda (d) (string-contains? "unclosed parenthesis" (list-ref d 3))) diagnoses)))))
+(test-end)
+
+(test-begin "cross-bracket-mismatch")
+(let ([doc (make-document "file:///test.ss" "(a [b (c x] y)" '())])
+  (call-with-port (open-file-output-port "/tmp/test-tokenizer/test.ss" (file-options replace))
+    (lambda (p) (put-bytevector p (string->utf8 "(a [b (c x] y)"))))
+  (source-file->annotations "(a [b (c x] y)" "/tmp/test-tokenizer/test.ss" 0 #t doc)
+  (let ([diagnoses (document-diagnoses doc)])
+    ; 1 original cross-bracket + 1 unclosed paren + 1 unclosed bracket
+    (test-equal 3 (length diagnoses))
+    (test-assert
+      (find (lambda (d) (string-contains? "parenthesized list terminated by bracket" (list-ref d 3))) diagnoses))
+    (test-assert
+      (find (lambda (d) (string-contains? "unclosed bracket" (list-ref d 3))) diagnoses))
+    (test-assert
+      (find (lambda (d) (string-contains? "unclosed parenthesis" (list-ref d 3))) diagnoses))))
+(test-end)
+
+(test-end)
+
+(exit (if (zero? (test-runner-fail-count (test-runner-get))) 0 1))

tests/analysis/test-tokenizer.sps
@@ -14,7 +14,7 @@
   (scheme-langserver analysis tokenizer))
 
 (test-begin "read ss")
-  (test-equal 16 (length (source-file->annotations "./run.ss")))
+  (test-equal 17 (length (source-file->annotations "./run.ss")))
 (test-end)
 
 (test-begin "read sps")

commit 3be042e3598484bd71ff9ea97f91b37e684a5ba9

revert(tokenizer): remove max-depth guard from fault-tolerant parsing
The max-depth parameter (default 10) was introduced in 6e7d309 to cap
tolerant-parse recursion, but it causes diagnostics to be generated
against patched source text rather than the original, leading to
redundant or misleading unclosed-paren reports.

Remove the parameter and the 'max tolerant depth exceeded' diagnostic
so that all diagnoses are based on the same source baseline.

All tokenizer tests pass (56 assertions).

analysis/tokenizer.sls
@@ -438,8 +438,6 @@
     ([source path start-position tolerant?]
       (source-file->annotations source path start-position tolerant? #f))
     ([source path start-position tolerant? maybe-document]
-      (source-file->annotations source path start-position tolerant? maybe-document 10))
-    ([source path start-position tolerant? maybe-document max-depth]
       (if (file-exists? path)
         (let ([port (open-string-input-port source)]
             [source-file-descriptor (make-source-file-descriptor path (open-file-input-port path))])
@@ -465,13 +463,7 @@
                                 (private:append-unclosed-paren-diagnoses maybe-document source msg))))))
                       (let ([after (private:tolerant-parse->patch source error-position)])
                         (if (= (string-length after) (string-length source))
-                          (if (> max-depth 0)
-                            (source-file->annotations after path start-position #t maybe-document (- max-depth 1))
-                            (begin
-                              (when maybe-document
-                                (append-new-diagnoses maybe-document
-                                  `(,error-position ,(+ error-position 1) 1 "Syntax error: max tolerant depth exceeded" "syntax" "syntax-error")))
-                              '()))
+                          (source-file->annotations after path start-position #t maybe-document)
                           (error 'tokenizer-error (condition-message e) (condition-irritants e)))))]
                   [(condition? e)
                     (let ([error-position (private:compute-error-position e port)])